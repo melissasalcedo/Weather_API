2796945055dd553287fa516cf34f447467327d0a
melissasalcedo <salcedomelissa189@gmil.com>
2024-02-07 01:43:53 +0800

    Handling the response

--- weather.py
+++ weather.py
@@ -8,5 +8,11 @@ def get_weather(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     response = requests.get(url)
 # Check if the response is successful
+    if response.status_code == 200:
+        data = response.json()
+        weather_description = data['weather'][0]['description']
+        temperature = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+        timestamp = data['dt']
 
 # The city to be inputed
