7a3abb2a639c3a226d155381d996c05c276807b3
melissasalcedo <salcedomelissa189@gmil.com>
2024-02-07 00:56:22 +0800

    Added pseudocode

---
+++ weather.py
@@ -0,0 +1,11 @@
+# Creating a program that will identify the weather in every city
+
+# Import what is needed
+
+# URL and KEY of API
+
+# Check if the response is successful
+
+# The city to be inputed
+
+# 
